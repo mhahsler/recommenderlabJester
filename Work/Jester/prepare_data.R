dfb264143fcabda1bfb1040d2057017074ff9ad8
Michael Hahsler <michael@hahsler.net>
2022-05-31 12:16:56 -0500

    updated to latest recommenderlab version.

--- Work/Jester/prepare_data.R
+++ Work/Jester/prepare_data.R
@@ -1,7 +1,6 @@
 library(recommenderlab)
 
-
-J1 <- read.csv("jester-data-1.csv", header=FALSE)
+J1 <- read.csv("jester-data-1.csv", header = FALSE)
 dim(J1)
 J1[J1 == 99] <- NA
 
@@ -14,25 +13,29 @@ J1[J1 == 99] <- NA
 #J3[J3 == 99] <- NA
 
 ### first row is number of votes
-Jester <- J1[,-1]
+Jester <- J1[, -1]
 #Jester <- rbind(J1,J2,J3)
 
 
 #Jester <- as(as.matrix(Jester+11), "realRatingMatrix")
 Jester <- as(as.matrix(Jester), "realRatingMatrix")
-colnames(Jester) <- paste('j',1:ncol(Jester),sep='')
-rownames(Jester) <- paste('u',1:nrow(Jester),sep='')
+colnames(Jester) <- paste('j', 1:ncol(Jester), sep = '')
+rownames(Jester) <- paste('u', 1:nrow(Jester), sep = '')
 
 
 
 ### read jokes "init<nr>.html"
-jokes <- lapply(1:100, function(i) readLines(paste("jokes/init",i,".html", sep="")))
+jokes <-
+  lapply(1:100, function(i)
+    readLines(paste("jokes/init", i, ".html", sep = "")))
 
 # get rid of first 5 lines (title)
-jokes <- lapply(jokes, "[", -c(1:5))
+jokes <- lapply(jokes, "[",-c(1:5))
 
-filter_tags <- function(txt) gsub("<[^>]*>", "", txt)
-filter_nbsp <- function(txt) gsub("&nbsp;", "", txt)
+filter_tags <- function(txt)
+  gsub("<[^>]*>", "", txt)
+filter_nbsp <- function(txt)
+  gsub("&nbsp;", "", txt)
 collapse <- function(txt) {
   txt <- paste(txt, collapse = " ")
   txt <- gsub("^\\s+|\\s+$", "", txt)
@@ -42,15 +45,15 @@ collapse <- function(txt) {
 jokes <- lapply(jokes, filter_tags)
 jokes <- lapply(jokes, filter_nbsp)
 jokes <- sapply(jokes, collapse)
-names(jokes) <- paste('j',1:length(jokes),sep='')
+names(jokes) <- paste('j', 1:length(jokes), sep = '')
 
 JesterJokes <- jokes
 
-save(Jester, JesterJokes, file="Jester.rda")
+save(Jester, JesterJokes, file = "../../data/Jester.rda")
 
 
 ### a 5k sample of Jester
 set.seed(1234)
 Jester5k <- sample(Jester, 5000)
 
-save(Jester5k, JesterJokes, file="Jester5k.rda")
+save(Jester5k, JesterJokes, file = "../../data/Jester5k.rda")
